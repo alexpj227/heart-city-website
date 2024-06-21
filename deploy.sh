c1f6af78e6726f397f6154b860b67ead2beffa4a
Clint <>
2024-06-21 09:35:13 -0700

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ npm install && \
 rm -rf build && \
 rm -rf docs && \
 npm run build && mv build docs && \
+echo -n "heartcitynow.com" > docs/CNAME && \
 git add . && git commit -m "Deploy" && \
 git push origin main;
