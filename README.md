e025abdca1337e36b236bb0a228ccaefaf92207e
Clint <>
2024-09-02 07:23:47 -0700

    Update readme and deploy to use heartcity.app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+## How to deploy to Production
+From the root directory of the project run:
+```
+> ./deploy.sh
+```
+
 ## Versions
 Node v18.20.4
--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ rm -rf build && \
 rm -rf docs && \
 npm run build && mv build docs && \
 cp docs/index.html docs/404.html && \
-echo -n "heartcitynow.com" > docs/CNAME && \
+echo -n "heartcity.app" > docs/CNAME && \
 git add . && git commit -m "Deploy" && \
 git push origin main;
