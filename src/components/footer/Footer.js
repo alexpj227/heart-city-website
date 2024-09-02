3c2f851493219b10d078d252cbab654e4f70a856
Clint <>
2024-09-02 08:11:04 -0700

    Update copy right year to auto update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,9 +3,10 @@ import styles from './Footer.module.css';
 import { PRIVACY_URL, TERMS_OF_USE_URL } from '../../App';
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
         <footer className={styles.footer}>
-            <p>&copy; 2024 Heart City. All rights reserved.</p>
+            <p>&copy; {currentYear} Heart City. All rights reserved.</p>
             <nav>
                 <a href={PRIVACY_URL}>Privacy Policy</a> | <a href={TERMS_OF_USE_URL}>Terms of Service</a>
             </nav>
